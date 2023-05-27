951aacc82d81657cb969b68825da56f5b9df722d
ejinbt <ejinbiju@gmail.com>
2023-05-27 13:29:18 +0530

    added update and delete

--- eventportal/core/views.py
+++ eventportal/core/views.py
@@ -1,15 +1,19 @@
 from flask import render_template, request, Blueprint
-
+from eventportal.models import Event
 core = Blueprint('core', __name__)
 
 
 @core.route('/')
 def index():
-    return render_template('index.html')
+    page = request.args.get('page',1,type=int)
+    events = Event.query.order_by(Event.event_date.desc()).paginate(page=page,per_page=10)
+    return render_template('index.html',events=events)
 
 @core.route("/admin")
 def admin():
-    return render_template('admin.html')
+    page = request.args.get('page',1,type=int)
+    events = Event.query.order_by(Event.event_date.desc()).paginate(page=page,per_page=10)
+    return render_template('admin.html',events=events)
 
 @core.route('/info')
 def info():
