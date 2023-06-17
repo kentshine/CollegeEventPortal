cf68de488d75b5c54da997f77f829be6e46cbd3a
kentshine <kenofficial18@gmai.com>
2023-06-17 12:06:00 +0530

    update

--- eventportal/core/views.py
+++ eventportal/core/views.py
@@ -1,18 +1,21 @@
 from flask import render_template, request, Blueprint
 from eventportal.models import Event
+from flask_login import current_user
 core = Blueprint('core', __name__)
 
 
 @core.route('/')
 def index():
-    page = request.args.get('page',1,type=int)
-    events = Event.query.order_by(Event.event_date.desc()).paginate(page=page,per_page=10)
+    page = request.args.get('page', 1, type=int)
+    events = Event.query.order_by(Event.event_date.asc()).paginate(page=page,per_page=10)
     return render_template('index.html',events=events)
+
+
 
 @core.route("/admin")
 def admin():
     page = request.args.get('page',1,type=int)
-    events = Event.query.order_by(Event.event_date.desc()).paginate(page=page,per_page=10)
+    events = Event.query.order_by(Event.event_date.asc()).paginate(page=page,per_page=4)
     return render_template('admin.html',events=events)
 
 
