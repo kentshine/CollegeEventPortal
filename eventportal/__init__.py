80ef94fabdea381eb2a161a3ac11a3223253103d
ejinbt <ejinbiju@gmail.com>
2023-06-25 13:14:42 +0530

    FINAL TESTINGS #1

--- eventportal/__init__.py
+++ eventportal/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail,Message
 from flask_login import LoginManager
+from eventportal import database
 
 
 
